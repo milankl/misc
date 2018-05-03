2c14a1d49b6d906f2032fd436b5daef432b67e64
milankl <milankloewer@gmx.de>
2018-05-03 16:33:47 +0100

    minors2

--- juliapython/lorenz.py
+++ juliapython/lorenz.py
@@ -1,42 +1,31 @@
 from time import time as tictoc
 
 def time_integration(N,x,y,z):
-    
-    # pre-allocate
-    dx,dy,dz = 0.,0.,0.
-    
+
+    dx,dy,dz = 0.,0.,0              # pre-allocate
+
     for i in range(N-1):
-        
-        # RHS
-        dx = dt*(sig*(y-x))
+
+        dx = dt*(sig*(y-x))         # RHS
         dy = dt*(x*(rho-z) - y)
         dz = dt*(x*y - beta*z)
-        
-        # Euler forward
-        x += dt*dx
+
+        x += dt*dx                  # Euler forward
         y += dt*dy
         z += dt*dz
-    
+
     return x,y,z
 
+N = int(1e8)                        # number of time steps
+dt = 0.01                           # time step
 
-N = int(1e8)        # number of time steps
-dt = 0.01   # time step
+x,y,z = 5.,5.,5.                    # initial conditions
 
-# initial conditions
-x,y,z = 5.,5.,5.    
-
-# L63 parameters
-sig = 10.
+sig = 10.                           # L63 parameters
 rho = 28.
 beta = 8./3.
 
-
-# RUN
-t0 = tictoc()
+t0 = tictoc()                       # RUN
 x,y,z = time_integration(N,x,y,z)
 t1 = tictoc()-t0
 print("time elapsed {:.2f}s".format(t1))
-
-print((x,y,z))
-
